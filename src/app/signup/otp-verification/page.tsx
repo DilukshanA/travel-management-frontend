9e1f9b7b63704fc015f7899ef52fe95a1dbd7581
Dilukshan <dilukshan2020da@gmail.com>
2025-06-18 20:33:30 +0530

    add error handling

--- src/app/signup/otp-verification/page.tsx
+++ src/app/signup/otp-verification/page.tsx
@@ -2,18 +2,69 @@
 import { OTP } from '@/components/ui/OTP'
 import { Box, Button } from '@mui/material';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
 const page = () => {
+
+  const router = useRouter();
 
     const [otp, setOtp] = useState('');
 
     const handleSubmit = async () => {
 
+
+      try {
+        if (!otp || otp.length !== 6) {
+          toast.error('Please enter a valid 6-digit OTP', {
+            duration: 3000,
+            position: 'top-right'
+          });
+          return;
+        }
+
         const response = await axios.post('http://localhost:4000/api/auth/signup-otp-verify', {
           email: localStorage.getItem('email'),
           otp: otp
         })
+
+        if (response.status === 200) {
+          toast.success('OTP verified successfully!', {
+            duration: 3000,
+            position: 'top-right'
+          });
+          localStorage.removeItem('email');
+          router.push('/');
+
+        } else if (response.status === 404) {
+          toast.error('OTP not found or Expired. Please try again.', {
+            duration: 3000,
+            position: 'top-right'
+          });
+
+        } else {
+          toast.error('Failed to verify OTP. Please try again.', {
+            duration: 3000,
+            position: 'top-right'
+          });
+
+        }
+      } catch (error) {
+
+        if (axios.isAxiosError(error)) {
+          toast.error('Failed to verify OTP. Please try again.', {
+            duration: 3000,
+            position: 'top-right'
+          });
+
+        } else if (error instanceof Error){
+          toast.error(error.message, { duration: 3000});
+          
+        } else {
+          toast.error('An unexpected error occurred. Please try again.', {duration: 3000});
+        }
+      }
     }
 
   return (
