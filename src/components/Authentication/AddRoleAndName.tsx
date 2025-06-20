bee5934e70f714e448455a139523fc75a41e3678
Dilukshan <dilukshan2020da@gmail.com>
2025-06-20 21:59:26 +0530

    add role after signup

---
+++ src/components/Authentication/AddRoleAndName.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const AddRoleAndName = () => {
+  return (
+    <Box>
+        <Box></Box>
+    </Box>
+  )
+}
+
+export default AddRoleAndName
