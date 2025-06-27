0f5d0159712dedd4f500e03c06a4733052c4d843
Dilukshan <dilukshan2020da@gmail.com>
2025-06-27 22:00:07 +0530

    create api call for otp verification

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApiSlice } from "./reducers/authApiSlice";
+import { otpApiSlice } from "./reducers/otpApiSlice";
 
 const store = configureStore({
     reducer: {
-        [authApiSlice.reducerPath] : authApiSlice.reducer
+        [authApiSlice.reducerPath] : authApiSlice.reducer,
+        [otpApiSlice.reducerPath] : otpApiSlice.reducer
     },
     // Adding the api middleware enables caching, invalidation, polling and other features of RTK Query
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware().concat(
-            authApiSlice.middleware
+            authApiSlice.middleware,
+            otpApiSlice.middleware
         );
     }
 })
