9987fb76ccadb5bcb78e00a8f6fe19465567ce10
Dilukshan <dilukshan2020da@gmail.com>
2025-06-29 15:17:30 +0530

    create userSlice with createSlice and dispatch it

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApiSlice } from "./reducers/authApiSlice";
 import { otpApiSlice } from "./reducers/otpApiSlice";
+import { userSlice } from "./reducers/userSlice";
 
 const store = configureStore({
     reducer: {
+        userSlice: userSlice.reducer,
         [authApiSlice.reducerPath] : authApiSlice.reducer,
         [otpApiSlice.reducerPath] : otpApiSlice.reducer,
     },
@@ -17,3 +19,9 @@ const store = configureStore({
 })
 
 export default store;
+
+// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
