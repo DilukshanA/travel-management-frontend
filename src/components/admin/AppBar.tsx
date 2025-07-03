8db51a427304e8dfb436cb268dc77dc0c23e1c64
Dilukshan <dilukshan2020da@gmail.com>
2025-07-03 13:27:25 +0530

    sperate dashboard

---
+++ src/components/admin/AppBar.tsx
@@ -0,0 +1,62 @@
+// components/AppBar.tsx
+"use client";
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export interface CustomAppBarProps extends MuiAppBarProps {
+  open: boolean;
+  onDrawerOpen: () => void;
+  drawerWidth: number;
+}
+
+const StyledAppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open' && prop !== 'drawerWidth',
+})<Pick<CustomAppBarProps, 'open' | 'drawerWidth'>>(
+  ({ theme, open, drawerWidth }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  })
+);
+
+export default function AppBar({
+  open,
+  onDrawerOpen,
+  drawerWidth,
+  ...rest
+}: CustomAppBarProps) {
+  return (
+    <StyledAppBar position="fixed" open={open} drawerWidth={drawerWidth} {...rest}>
+      <Toolbar>
+        {!open && (
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={onDrawerOpen}
+            edge="start"
+            sx={{ marginRight: 5 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </StyledAppBar>
+  );
+}
