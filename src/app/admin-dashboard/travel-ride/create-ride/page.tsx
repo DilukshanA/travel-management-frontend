d09af248e8a23fa0ec9b8ec2b5a9d8e018092f03
Dilukshan <dilukshan2020da@gmail.com>
2025-07-07 20:24:15 +0530

    create ride contoller

--- src/app/admin-dashboard/travel-ride/create-ride/page.tsx
+++ src/app/admin-dashboard/travel-ride/create-ride/page.tsx
@@ -3,27 +3,26 @@ import RideForm from '@/components/create-ride/RideForm'
 import LoadingBackdrop from '@/components/ui/LoadingBackdrop'
 import { useCreateRideMutation } from '@/redux/reducers/rideApiSlice'
 import { Ride } from '@/types/ride'
-import { CircularProgress } from '@mui/material'
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 
 const page = () => {
 
-  // const [trips, setTrips] = useState<Ride[]>([])
-
   const [ createRide, { isLoading, isSuccess, isError, error } ] = useCreateRideMutation();
-
+  
   const handleCreateRide = async (trip: Ride) => {
-  // const updatedTrips = [...trips, trip]
-  // setTrips(updatedTrips)
-  // localStorage.setItem("travel-trips", JSON.stringify(updatedTrips))
+ 
   try {
     console.log("Creating ride with trip data:", trip);
     const result = await createRide(trip).unwrap();
     toast.success("Ride created successfully!");
     console.log(result);
-  } catch (error) {
-    console.log("Error creating ride:", error);
+  } catch (err : any) {
+    if (err?.OriginalStatus === 404) {
+      toast.error("Server problem occurred!");
+    } else {
+      toast.error('An unexpected error occurred !');
+    }
   }
 }
 
