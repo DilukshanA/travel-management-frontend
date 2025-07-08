c600b70a0cd414cb85b72d87ab864c21b291a1e3
Dilukshan <dilukshan2020da@gmail.com>
2025-07-08 19:59:19 +0530

    add vehicle field

--- src/components/vehicle/AddVehicleForm.tsx
+++ src/components/vehicle/AddVehicleForm.tsx
@@ -1,8 +1,44 @@
+"use client"
+import { Box, TextField } from '@mui/material'
 import React from 'react'
+import BasicSelectField from '../ui/BasicSelectField'
 
 const AddVehicleForm = () => {
   return (
-    <div>AddVehicleForm</div>
+    <Box sx={{ width: '100%', maxWidth: 600, margin: 'auto', padding: 2 }}>
+        <Box display='flex' gap={2}>
+            <TextField
+                label="Vehicle Name"
+                placeholder='Enter vehicle name'
+                variant="outlined"
+                fullWidth
+            />
+            <TextField
+                label="Vehicle Type"
+                placeholder='Enter vehicle type'
+                variant="outlined"
+                fullWidth
+            />
+        </Box>
+        <Box display='flex' gap={2} mt={2}>
+            <TextField
+                label="Vehicle Number"
+                placeholder='Enter vehicle number'
+                variant="outlined"
+                fullWidth
+            />
+            <BasicSelectField
+                name="vehicleStatus"
+                fieldName="Vehicle Status"
+                names={[
+                    { label: 'Active', value: 'active' },
+                    { label: 'Inactive', value: 'inactive' },
+                ]}
+                value="active"
+                onChange={(value) => console.log(value)}
+            />
+        </Box>
+    </Box>
   )
 }
 
